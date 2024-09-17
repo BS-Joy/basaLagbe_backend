7b3237e4e89a3c67d007cb2f14a711c73d5c2401
BS-Joy <badhansamadder5@gmail.com>
2024-09-17 19:48:25 +0600

    added endpoint for delete a bookmarked ad

--- controllers/bookmarkController.js
+++ controllers/bookmarkController.js
@@ -1,16 +1,20 @@
 import mongoose from "mongoose";
 import Bookmark from "../models/bookmarkModel.js";
+import Ads from "../models/adsModel.js";
+import Categories from "../models/categoryModel.js";
 
 export const addToBookmark = async (req, res) => {
   try {
     const { userId } = req.params;
     const { adId } = req.body;
     const mongoAdId = new mongoose.Types.ObjectId(adId);
-    // console.log(mongoAdId);
 
     const result = await Bookmark.findOneAndUpdate(
       { userId },
-      { $addToSet: { adIds: mongoAdId } },
+      {
+        $addToSet: { adIds: mongoAdId },
+        $inc: { totalAds: 1 }, // Increment totalAds by 1
+      },
       { new: true, upsert: true }
     );
 
@@ -24,13 +28,54 @@ export const checkAdBookmarkStatus = async (req, res) => {
   try {
     const { userId, adId } = req.params;
 
-    // const mongoAdId = new mongoose.Types.ObjectId(adId);
-
     const bookmark = await Bookmark.findOne({ userId: userId }).lean();
 
     const isBookmarked = bookmark.adIds.some((id) => id.toString() === adId);
 
     res.status(200).json(isBookmarked);
+  } catch (err) {
+    res.status(500).json({ error: "Internal server error!" });
+  }
+};
+
+export const getBookmarksByUser = async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    const bookmarks = await Bookmark.findOne({ userId: userId })
+      .populate({
+        path: "adIds",
+        model: Ads,
+        populate: {
+          path: "category",
+          model: Categories,
+        },
+      })
+      .lean();
+
+    res.status(200).json(bookmarks);
+  } catch (err) {
+    res.status(500).json({ error: "Internal server error!" });
+  }
+};
+
+export const deleteABookmark = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const { adId } = req.body;
+
+    const mongoAdId = new mongoose.Types.ObjectId(adId);
+
+    const result = await Bookmark.findOneAndUpdate(
+      { userId },
+      {
+        $pull: { adIds: mongoAdId },
+        $inc: { totalAds: -1 }, // Decrement totalAds by 1
+      },
+      { new: true }
+    );
+
+    res.status(200).json(result);
   } catch (err) {
     res.status(500).json({ error: "Internal server error!" });
   }
--- routes/bookmarkRoutes.js
+++ routes/bookmarkRoutes.js
@@ -2,11 +2,15 @@ import express from "express";
 import {
   addToBookmark,
   checkAdBookmarkStatus,
+  deleteABookmark,
+  getBookmarksByUser,
 } from "../controllers/bookmarkController.js";
 
 const bookmarkRouter = express.Router();
 
 bookmarkRouter.put("/:userId", addToBookmark);
 bookmarkRouter.get("/:userId/:adId", checkAdBookmarkStatus);
+bookmarkRouter.get("/:userId", getBookmarksByUser);
+bookmarkRouter.delete("/:userId", deleteABookmark);
 
 export default bookmarkRouter;
